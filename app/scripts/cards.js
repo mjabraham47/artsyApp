a5927bfcd0d188b9df033f1e143edef6f7fbe487
Michael J. Abraham <mjabraham@gmail.com>
2015-01-23 15:39:17 -0500

    not great

--- app/scripts/cards.js
+++ app/scripts/cards.js
@@ -12,47 +12,62 @@ app
 })
 .controller('CardsCtrl', function($scope, $http) {
     var cardTypes;
-    var usedCards = [];
-        //     {  title: 'So much grass #hippster'},
-    //     {  title: 'Way too much Sand, right?'},
-    //     {  title: 'Beautiful sky from wherever'},
-    // ];
-    
     $scope.cards = [];
+    $scope.viewedCards = [];
+
 
     $http.get('http://localhost:3000/artworks')
         .success(function(data){
-                    console.log(data)
-
-            cardTypes = data._embedded.artworks
+            cardTypes = data._embedded.artworks;
             for(var i = 0; i < cardTypes.length; i++) {
-                $scope.addCard();
+                $scope.addCards();
             }
         })
 
-    $scope.addCard = function() {
+    $scope.addCards = function() {
         var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
         $scope.cards.push(angular.extend({}, newCard));
     }
 
     $scope.cardSwipedLeft = function(index) {
         console.log('Left swipe');
+        $scope.viewedCards.push($scope.cards[index])
+        console.log($scope.viewedCards);
+            // for (var i=0; i<$scope.cards.length; i++) {
+            // for (var j=0; j<$scope.viewedCards.length; j++){
+            //     if ($scope.cards[i] === $scope.viewedCards[j]) {
+            //         $scope.cards.splice(i,1)
+            //     }
+            // }
+        // }
     }
+
  
     $scope.cardSwipedRight = function(index) {
-        console.log('Right swipe');
+    console.log('Right swipe');
+    $scope.viewedCards.push($scope.cards[index]);
     $http.get('http://localhost:3000/related/' +$scope.cards[index].id)
         .success(function(data){
-                for (var i=0; i<data._embedded.artworks.length; i++){
-                    $scope.cards.push(data._embedded.artworks[i])
-                }
-            })
-
+            console.log(data)
+            console.log("TEST index: ", index);
+            console.log("VIEWED: ", $scope.viewedCards);
+            for (var i=0; i<data._embedded.artworks.length; i++){
+                $scope.cards.push(data._embedded.artworks[i])
+            }
+            // for (var i=0; i<$scope.cards.length; i++) {
+            // if (JSON.stringify($scope.cards[i]) === JSON.stringify($scope.viewedCards[index])) {
+            //     console.log(JSON.stringify($scope.cards[i]) === JSON.stringify($scope.viewedCards[index]))
+            //     $scope.cards.splice(i,1)
+            //     console.log('spliced')
+            //     }        
+        // }
+        })
     }
  
     $scope.cardDestroyed = function(index) {
         $scope.cards.splice(index, 1);
         console.log('Card removed');
+
     }
 
     // $http.get("http://localhost:3000", function(err,data) {
