dba881cffe70a9d8c381a70721729c9cc8e4f245
Michael J. Abraham <mjabraham@gmail.com>
2015-01-23 16:53:30 -0500

    cheese

--- app/scripts/cards.js
+++ app/scripts/cards.js
@@ -13,11 +13,12 @@ app
 .controller('CardsCtrl', function($scope, $http) {
     var cardTypes;
     $scope.cards = [];
-    $scope.viewedCards = [];
 
 
     $http.get('http://localhost:3000/artworks')
-        .success(function(data){
+        .then(function(data){
+            return data.data
+        }).then(function(data){
             cardTypes = data._embedded.artworks;
             for(var i = 0; i < cardTypes.length; i++) {
                 $scope.addCards();
@@ -30,51 +31,38 @@ app
     }
 
     $scope.cardSwipedLeft = function(index) {
-        console.log('Left swipe');
-        $scope.viewedCards.push($scope.cards[index])
-        console.log($scope.viewedCards);
-            // for (var i=0; i<$scope.cards.length; i++) {
-            // for (var j=0; j<$scope.viewedCards.length; j++){
-            //     if ($scope.cards[i] === $scope.viewedCards[j]) {
-            //         $scope.cards.splice(i,1)
-            //     }
-            // }
-        // }
+        if($scope.cards.length < 3) {
+        $http.get('http://localhost:3000/artworks/more')
+        .then(function(data){
+            return data.data
+        }).then(function(data){
+            cardTypes = data._embedded.artworks;
+            for(var i = 0; i < cardTypes.length; i++) {
+                $scope.addCards();
+            }
+        })
+      }
+      console.log('Cards:', $scope.cards.length)
     }
 
  
     $scope.cardSwipedRight = function(index) {
-    console.log('Right swipe');
-    $scope.viewedCards.push($scope.cards[index]);
-    $http.get('http://localhost:3000/related/' +$scope.cards[index].id)
-        .success(function(data){
-            console.log(data)
-            console.log("TEST index: ", index);
-            console.log("VIEWED: ", $scope.viewedCards);
-            for (var i=0; i<data._embedded.artworks.length; i++){
-                $scope.cards.push(data._embedded.artworks[i])
-            }
-            // for (var i=0; i<$scope.cards.length; i++) {
-            // if (JSON.stringify($scope.cards[i]) === JSON.stringify($scope.viewedCards[index])) {
-            //     console.log(JSON.stringify($scope.cards[i]) === JSON.stringify($scope.viewedCards[index]))
-            //     $scope.cards.splice(i,1)
-            //     console.log('spliced')
-            //     }        
-        // }
-        })
+        $http.get('http://localhost:3000/related/' +$scope.cards[index].id)
+            .then(function(data){
+                return data.data
+            }).then(function(data){
+                for (var i=0; i<data._embedded.artworks.length; i++){
+                    $scope.cards.push(data._embedded.artworks[i])
+                }
+            })
+        console.log('Cards:', $scope.cards.length)
     }
  
     $scope.cardDestroyed = function(index) {
         $scope.cards.splice(index, 1);
-        console.log('Card removed');
-
     }
 
-    // $http.get("http://localhost:3000", function(err,data) {
-    //     console.log(err, data);
-    // });
-
-
+ 
 
 
 })
