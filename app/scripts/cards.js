1a0b0e49eaba1bfd60da9df5a6acb0941558bcc5
KamillaKhabibrakhmanova <venessiel@gmail.com>
2015-01-25 22:41:49 -0500

    added three way data binding for swipes

--- app/scripts/cards.js
+++ app/scripts/cards.js
@@ -12,95 +12,76 @@ app
 })
 .controller('CardsCtrl', function($scope, $http, $firebase) {
 
-
-    //var sync = $firebase(seeds);
-    //var matches = $firebase(images);
-    //$scope.cards = sync.$asArray();
-    //$scope.matches = sync.$asArray();
     var cardTypes;
     $scope.addCards;
     $scope.holdTheCards = [];
     $scope.likedCards = [];
-    $scope.cards = [];
+
 
     var ref = new Firebase("https://swipe-artsy.firebaseio.com");
     var seeds = new Firebase("https://swipe-artsy.firebaseio.com/seeds");
     var authData = ref.getAuth();
+
+//three way binding for user's images array
+    var fb_images = new Firebase('https://swipe-artsy.firebaseio.com/' + authData.facebook.id.toString() + '/images');
     var image_path = authData.facebook.id.toString() + '/images';
+    var image_sync = $firebase(fb_images);
+    var images_array = image_sync.$asArray();
+    //wait for images to load before adding to scope
+    images_array.$loaded(function() {
+      $scope.cards = images_array;
+    });
+
+//three way binding for user's favorites
+    var fb_faves = new Firebase('https://swipe-artsy.firebaseio.com/' + authData.facebook.id.toString() + '/favorites');
+    var faves_sync = $firebase(fb_faves);
+    var faves_array = faves_sync.$asArray();
+    $scope.faves = faves_array;
+
     var liked_path = authData.facebook.id.toString() + '/favorites';
     var seeds_path = authData.facebook.id.toString() + '/seeds';
 
     //check if seeds exists if not copy
     var users_location = 'htts://swipe-artsy.firebaseio.com/' + authData.facebook.id.toString();
 
-
-    // var userRef = new Firebase(users_location);
-    // ref.child(seeds_path).remove(function(){
-    //   console.log('seeds removed');
-    // });
-    ref.child(image_path).once('value', function(snapshot) {
-      if ( snapshot.val() === null ) {
+    //wait for firebase to load images, if there are none add seeds
+    images_array.$loaded(function() {
+      if ( $scope.cards.length === 0 ) {
         console.log('seeds do not exist');
         seeds.once('value', function(data) {
           data.forEach(function(child) {
-            //ref.child(seeds_path).push(child.val());
-            $scope.cards.push(child.val());
-
+            $scope.cards.$add(child.val());
           });
-          ref.child(image_path).push(data.val());
-          console.log($scope.cards);
         });
       } else {
         console.log('seeds exist');
-        ref.child(image_path).on('value', function(data) {
-          data.forEach(function(child) {
-            $scope.cards.push(child.val());
-          })
-        })
       }
-    });
+   });
 
-    $scope.cardSwipedLeft = function(index) {
-        if($scope.cards.length < 50) {
-          ref.child(image_path).orderByKey().limitToFirst(10).on('value', function(data) {
-            data.forEach(function(card) {
-              $scope.cards.push(card.val());
-            });
-          });
-        }
+// swipe left doesn't do anything anymore
+    $scope.cardSwipedLeft = function(card) {
       console.log('Cards:', $scope.cards.length)
     }
 
+//swipe right - add card to user's favorites array and look up similar works
+    $scope.cardSwipedRight = function(card) {
+      console.log(card);
 
-    $scope.cardSwipedRight = function(index) {
-        $scope.likedCards.push($scope.cards[index].id);
-        var liked_add = angular.copy($scope.cards[index]);
-        ref.child(liked_path).push(liked_add);
-        if($scope.cards.length < 50) {
-          ref.child(image_path).orderByKey().limitToFirst(10).on('value', function(data) {
-            data.forEach(function(card) {
-              $scope.cards.push(card.val());
-              var delete_path = image_path + card.key().toString();
-              ref.child(delete_path).remove();
-            });
-          });
-        }
-
-
-        $http.get('http://localhost:3000/related/' + authData.facebook.id +'/' +$scope.cards[index].id)
+        $scope.faves.$add(card);
+        console.log('faves', $scope.faves);
+//call server to get similar artworks and add to users' images
+        $http.get('http://localhost:3000/related/' + authData.facebook.id +'/' + card.id)
             .then(function(data){
                 console.log(data);
             });
-            // .then(function(data){
-            //     $scope.holdTheCards = data;
-            //     $scope.cards = $scope.holdTheCards._embedded.artworks.concat($scope.cards)
-            //     console.log($scope.cards);
-            // })
+
         console.log('Cards:', $scope.cards.length)
     }
+//remove card from users' images array when swiped
+    $scope.cardDestroyed = function(card) {
+      console.log('removed', card);
+      $scope.cards.$remove(card);
 
-    $scope.cardDestroyed = function(index) {
-      $scope.cards.splice(index,1);
     }
 
 
