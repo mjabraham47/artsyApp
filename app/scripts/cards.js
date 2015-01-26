33ba49e376c61afc2fc8450945dc75c0aa7de8c7
Michael J. Abraham <mjabraham@gmail.com>
2015-01-26 14:05:40 -0500

    capital letter in url

--- app/scripts/cards.js
+++ app/scripts/cards.js
@@ -1,15 +1,15 @@
 'use strict';
 app
-.directive('noScroll', function() {
-    return {
-        restrict: 'A',
-        link: function($scope, $element, $attr) {
-            $element.on('touchmove', function(e) {
-                e.preventDefault();
-            });
-        }
-    };
-})
+// .directive('noScroll', function() {
+//     return {
+//         restrict: 'A',
+//         link: function($scope, $element, $attr) {
+//             $element.on('touchmove', function(e) {
+//                 e.preventDefault();
+//             });
+//         }
+//     };
+// })
 .controller('CardsCtrl', function($scope, $http, $firebase) {
 
     var cardTypes;
