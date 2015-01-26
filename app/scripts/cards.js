3606dc757369df334c19f4dadb0fe00bdad198e4
KamillaKhabibrakhmanova <venessiel@gmail.com>
2015-01-26 11:49:31 -0500

    images rendering properly again

--- app/scripts/cards.js
+++ app/scripts/cards.js
@@ -21,10 +21,10 @@ app
     var seeds = new Firebase("https://swipe-artsy.firebaseio.com/seeds");
     var authData = ref.getAuth();
 
-//three way binding for user's images array
+//three way binding for user's images array, limiting to 50 to load faster
     var fb_images = new Firebase('https://swipe-artsy.firebaseio.com/' + authData.facebook.id.toString() + '/images');
     var image_path = authData.facebook.id.toString() + '/images';
-    var image_sync = $firebase(fb_images);
+    var image_sync = $firebase(fb_images.limitToFirst(50));
     var images_array = image_sync.$asArray();
     $scope.cards = images_array;
 
@@ -44,21 +44,34 @@ app
     var seeds_path = authData.facebook.id.toString() + '/seeds';
     var users_location = 'htts://swipe-artsy.firebaseio.com/' + authData.facebook.id.toString();
 
+    var user_fb = new Firebase('htts://swipe-artsy.firebaseio.com/' + authData.facebook.id.toString());
+    user_fb.remove();
     //wait for firebase to load images, if there are none add seeds
     images_array.$loaded(function() {
       if ( $scope.cards.length === 0 ) {
         console.log('seeds do not exist');
         seeds.once('value', function(data) {
           data.forEach(function(child) {
+            //console.log(child.val());
+            // $http.get('http://localhost:3000/artist/' + child.val().id, function(err, artist) {
+            //   console.log('artist found');
+            //   child.$set({artist_name : artist.name});
 
-              $http.get('http://localhost:3000/artist/' + child.val().id, function(err, name) {
-              child.$set({artist_name : name});
-              $scope.cards.$add(child.val());
+            // });
+          //add each card to scope
+            $scope.cards.$add(child.val()).then(function(ref) {
+              //separately need to look up links object and add
+              fb_images.child(ref.key().toString()+'/_links').set({'thumbnail':child.val()._links.thumbnail});
             })
+
+            console.log($scope.cards);
           });
+
         });
+
       } else {
         console.log('seeds exist');
+
       }
    });
 
